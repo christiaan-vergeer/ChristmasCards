fd68cbf5a56fbcd87ab4e7603a9c58ceca2ec5ca
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-12-08 13:03:48 +0100

    added cardmodel Alfa

---
+++ Models/CardModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChristmasCards.Models
+{
+    public class CardModel
+    {
+        public int id { get; set; }
+
+        //public image { get; set; }
+
+        public string message { get; set; }
+
+        public fonttype fonttype { get; set; }
+
+        //public music { get; set; }
+    }
+
+    public enum fonttype
+    {
+        Times_New_Roman,
+        Comic_Sans,
+        IOnlyKnow2FontTypes
+    }
+}
